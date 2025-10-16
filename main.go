3d7c131d4526253d937ee21c626df07c17a463d3
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-10-16 16:43:23 +0330

    add http server and user register handler

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gameapp/repository/mysql"
+	"gameapp/service/userservice"
+	"io"
+	"net/http"
 )
 
 func main() {
-	mysqlRepo := mysql.New()
-	is, err := mysqlRepo.IsPhoneNumberUnique("0914")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(is)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/users/register", func(writer http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			fmt.Fprintln(writer, "invalid method")
+			return
+		}
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintln(writer, err)
+			return
+		}
+		var req userservice.RegisterRequest
+		err = json.Unmarshal(data, &req)
+		if err != nil {
+			fmt.Fprintln(writer, err)
+			return
+		}
+		repo := mysql.New()
+		_, err = userservice.New(repo).Register(req)
+		if err != nil {
+			fmt.Fprintln(writer, err)
+			return
+		}
+		fmt.Fprintln(writer, "user created")
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintln(w, "everything is good!")
+	})
+
+	http.ListenAndServe("localhost:8080", mux)
 }
